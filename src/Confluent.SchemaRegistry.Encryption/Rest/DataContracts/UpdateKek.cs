f1c9afc2f3265194da631edd0958433d7b81aaaf
Robert Yokota <rayokota@gmail.com>
2024-04-30 15:06:11 -0700

    Add dek client

---
+++ src/Confluent.SchemaRegistry.Encryption/Rest/DataContracts/UpdateKek.cs
@@ -0,0 +1,43 @@
+// Copyright 2024 Confluent Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Refer to LICENSE for more information.
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Confluent.SchemaRegistry.Encryption
+{
+    [DataContract]
+    public class UpdateKek
+    {
+        /// <summary>
+        ///     The KMS properties.
+        /// </summary>
+        [DataMember(Name = "kmsProps")]
+        public new IDictionary<string, string> KmsProps { get; set; }
+
+        /// <summary>
+        ///     The doc for the KEK.
+        /// </summary>
+        [DataMember(Name = "doc")] 
+        public new string Doc { get; set; }
+        
+        /// <summary>
+        ///     Whether the KEK is shared.
+        /// </summary>
+        [DataMember(Name = "shared")]
+        public new bool Shared { get; set; }
+    }
+}
